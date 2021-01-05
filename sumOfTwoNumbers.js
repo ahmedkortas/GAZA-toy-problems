35b0e91df603e2047dcb913a899ed245adfae212
Ahmed Kortas <42817639+ahmedkortas@users.noreply.github.com>
2021-01-05 15:13:02 +0100

    first toy problem

---
+++ sumOfTwoNumbers.js
@@ -0,0 +1,17 @@
+// Create a function that takes two numbers as arguments and return their sum.
+
+// addition(3, 2) ➞ 5
+
+// addition(-3, -6) ➞ -9
+
+// addition(7, 3) ➞ 10
+
+// Notes
+// Don't forget to return the result.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+function some(firstNumber, secondNumber) {
+  //your code goes here
+  return;
+}
