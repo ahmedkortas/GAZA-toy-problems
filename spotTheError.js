f1dc48353eed07284504710b4d9307f9104782ff
Ahmed Kortas <42817639+ahmedkortas@users.noreply.github.com>
2021-01-09 11:33:34 +0100

    second toy problem

---
+++ spotTheError.js
@@ -0,0 +1,19 @@
+/**
+ * in order to calculate the factorial of 5 we need to do the following
+ * 5*4*3*2*1
+ */
+
+/**
+ * @param {number}
+ * @returns {number}
+ */
+
+//this code contains some problems please fix them
+
+const factorial = (n) => {
+  var fact = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    fact = fact * i;
+  }
+  return fact;
+};
